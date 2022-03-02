2622577fef5f83565b940b7b466337e0239c6d69
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2022-03-02 13:04:02 -0700

    Initial commit

---
+++ ModFrame/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+
+namespace ModFrame
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class NewMod : BaseUnityPlugin
+    {
+        private const string ModName = "New Mod";
+        private const string ModVersion = "1.0";
+        private const string ModGUID = "some.new.guid";
+        private static Harmony harmony = null!;
+        
+        public void Awake()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            harmony = new(ModGUID);
+            harmony.PatchAll(assembly);
+        }
+        
+        
+        public void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+    }
+}
